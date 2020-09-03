39621569ead0817385e5df3bb8feeced9aea8e90
sqy19930221 <shen_qyu@163.com>
2020-09-03 22:18:22 +0800

    增加方法

--- src/main/java/com/shen/TestGit.java
+++ src/main/java/com/shen/TestGit.java
@@ -4,4 +4,8 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("测试git");
     }
+
+    public void show() {
+        System.out.println("哈哈");
+    }
 }
