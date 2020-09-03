f33043e2229c36cdcc474935f080fa1af92603fe
sqy19930221 <shen_qyu@163.com>
2020-09-03 22:16:44 +0800

    增加list

--- src/main/java/com/shen/TestGit.java
+++ src/main/java/com/shen/TestGit.java
@@ -1,7 +1,11 @@
 package com.shen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("测试git");
+        List<String> list = new ArrayList<>();
     }
 }
