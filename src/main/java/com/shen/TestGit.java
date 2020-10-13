4dbf49d3b20409abbcf6de9b3b94eb82f9547612
sqy19930221 <shen_qyu@163.com>
2020-10-13 19:58:39 +0800

    修改为王佳

--- src/main/java/com/shen/TestGit.java
+++ src/main/java/com/shen/TestGit.java
@@ -7,7 +7,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("测试git");
         List<String> list = new ArrayList<>();
-        System.out.println("我是王佳");
+        System.out.println("我是沈庆宇");
     }
 
     public void show() {
